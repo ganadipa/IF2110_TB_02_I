733c7d1d4a0a1acf2d163679746c87e5124791bb
InTheRain <indrswara@gmail.com>
2023-11-02 04:41:09 +0700

    feat: update draf kicau

---
+++ lib/Types/DrafKicauan.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include "./DrafKicauan.h"
+
+
+void InisialisasiDrafKicau(Kicauan *k, int ID, String user, String teks){
+    IDKicau(*k) = ID; 
+    userKicau(*k) = user; 
+    likeKicau(*k) = 0; 
+    setText(k, teks);
+}
+
+void SimpanDraf(Kicauan *k){
+    Draf draf; 
+
+
+}
