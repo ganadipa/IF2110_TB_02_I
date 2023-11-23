09b04ea56dfddeb97dd9209ff04db5764a3f69ac
InTheRain <indrswara@gmail.com>
2023-11-23 15:08:22 +0700

    fix: pengguna.config no space in endfile

--- lib/Types/Application/simpan-dan-muat/simpan.c
+++ lib/Types/Application/simpan-dan-muat/simpan.c
@@ -9,13 +9,13 @@ boolean isExist(String *path){
 void tulisGrafPertemanan(FILE* filepath, Graf graf){
     int i, j;
     for (i = 0; i < NUMVERTICES(graf); i++) {
+        fprintf(filepath, "\n");
         for (j = 0; j < NUMVERTICES(graf); j++) {
             fprintf(filepath, "%d", CONNECTED(graf, i , j));
             if(j != NUMVERTICES(graf) - 1) {
                 fprintf(filepath, " ");
             }
         }
-        fprintf(filepath, "\n");
     }
 }
 
@@ -29,52 +29,50 @@ void SavingFilePengguna(String* path, ListUser user, Application app){
     FILE *filePengguna = fopen(parentDir.buffer, "w");
     int i = 0; 
     int totalPermintaan = 0;
-    fprintf(filePengguna, "%d\n", user.length);
-    for(i = 0; i < user.length; i++){
-        deleteRest(&user.contents[i].name);
-        deleteRest(&user.contents[i].password);
-        deleteRest(&user.contents[i].profile.bio);
-        deleteRest(&user.contents[i].profile.phoneNumber);
-        deleteRest(&user.contents[i].profile.weton);
-        fprintf(filePengguna, "%s\n", user.contents[i].name.buffer);
-        fprintf(filePengguna, "%s\n", user.contents[i].password.buffer);
-        fprintf(filePengguna, "%s\n", user.contents[i].profile.bio.buffer);
-        fprintf(filePengguna, "%s\n", user.contents[i].profile.phoneNumber.buffer);
-        fprintf(filePengguna, "%s\n", user.contents[i].profile.weton.buffer);
-        if(user.contents[i].profile.isPrivate){
-            fprintf(filePengguna, "Privat\n");
-        } else fprintf(filePengguna, "Publik\n");
+    fprintf(filePengguna, "%d", user.length);
+    if(user.length > 0){
+        for(i = 0; i < user.length; i++){
+            deleteRest(&user.contents[i].name);
+            deleteRest(&user.contents[i].password);
+            deleteRest(&user.contents[i].profile.bio);
+            deleteRest(&user.contents[i].profile.phoneNumber);
+            deleteRest(&user.contents[i].profile.weton);
+            fprintf(filePengguna, "\n%s", user.contents[i].name.buffer);
+            fprintf(filePengguna, "\n%s", user.contents[i].password.buffer);
+            fprintf(filePengguna, "\n%s", user.contents[i].profile.bio.buffer);
+            fprintf(filePengguna, "\n%s", user.contents[i].profile.phoneNumber.buffer);
+            fprintf(filePengguna, "\n%s", user.contents[i].profile.weton.buffer);
+            if(user.contents[i].profile.isPrivate){
+                fprintf(filePengguna, "\nPrivat");
+            } else fprintf(filePengguna, "\nPublik");
 
-        int row = user.contents[i].profile.photo.color.rowEff;
-        int col = user.contents[i].profile.photo.color.colEff;
-        int j, k;
+            int row = user.contents[i].profile.photo.color.rowEff;
+            int col = user.contents[i].profile.photo.color.colEff;
+            int j, k;
 
-        for(j = 0; j < row; j++){
-            for(k = 0; k < col; k++){
-                fprintf(filePengguna, "%c %c ", user.contents[i].profile.photo.color.mem[j][k], user.contents[i].profile.photo.symbols.mem[j][k]);
+            for(j = 0; j < row; j++){
+                fprintf(filePengguna, "\n");
+                for(k = 0; k < col; k++){
+                    fprintf(filePengguna, "%c %c ", user.contents[i].profile.photo.color.mem[j][k], user.contents[i].profile.photo.symbols.mem[j][k]);
+                }
+                
             }
-            fprintf(filePengguna, "\n");
-
+            totalPermintaan += lengthRequestQueue(user.contents[i].friendRequest);
+            //tulis matrix jumlah permintaan teman 
         }
-        totalPermintaan += lengthRequestQueue(user.contents[i].friendRequest);
-        //tulis matrix jumlah permintaan teman 
-    }
-    tulisGrafPertemanan(filePengguna, FRIENDSHIPS(app));
-    fprintf(filePengguna, "%d\n", totalPermintaan);
-    int count = 0; 
-    for(i = 0; i < user.length; i++){
-        if(lengthRequestQueue(user.contents[i].friendRequest) != 0){
-            int j;
-            User u = user.contents[i];
-            RequestQueue rq = FRIEND_REQUEST(u);
-            int IDaccept = u.ID;
-            for(j = 0; j < lengthRequestQueue(rq); j++){
-                count++;
-                int IDrequest = ELMT_ReqQue(rq, j).userID;
-                if(totalPermintaan != count){
-                    fprintf(filePengguna, "%d %d %d\n", IDrequest, IDaccept, user.contents[IDrequest].friendCount);
-                } else{
-                    fprintf(filePengguna, "%d %d %d", IDrequest, IDaccept, user.contents[IDrequest].friendCount);
+        tulisGrafPertemanan(filePengguna, FRIENDSHIPS(app));
+        fprintf(filePengguna, "\n%d", totalPermintaan);
+        int count = 0; 
+        for(i = 0; i < user.length; i++){
+            if(lengthRequestQueue(user.contents[i].friendRequest) != 0){
+                int j;
+                User u = user.contents[i];
+                RequestQueue rq = FRIEND_REQUEST(u);
+                int IDaccept = u.ID;
+                for(j = 0; j < lengthRequestQueue(rq); j++){
+                    count++;
+                    int IDrequest = ELMT_ReqQue(rq, j).userID;
+                    fprintf(filePengguna, "\n%d %d %d", IDrequest, IDaccept, user.contents[IDrequest].friendCount);
                 }
             }
         }
@@ -89,20 +87,16 @@ void SavingFileKicauan(String* path, ListKicau kicauan, ListUser user){
     snprintf(Dir.buffer, 1000, "%s/%s", path->buffer, kicau);
     FILE *fileKicauan = fopen(Dir.buffer, "w"); 
     int i = 0; 
-    fprintf(fileKicauan, "%d\n", kicauan.nEff);
+    fprintf(fileKicauan, "%d", kicauan.nEff);
     for(i = 0; i < kicauan.nEff; ++i){
         deleteRest(&kicauan.buffer[i].text);
         deleteRest(&user.contents[kicauan.buffer[i].IDuser].name);
         deleteRest(&kicauan.buffer[i].dateTime);
-        fprintf(fileKicauan, "%d\n", kicauan.buffer[i].IDKicau);
-        fprintf(fileKicauan, "%s\n", kicauan.buffer[i].text.buffer); 
-        fprintf(fileKicauan, "%d\n", kicauan.buffer[i].like);
-        fprintf(fileKicauan, "%s\n", user.contents[kicauan.buffer[i].IDuser].name.buffer); 
-        if (i == kicauan.nEff -1) {
-            fprintf(fileKicauan, "%s", kicauan.buffer[i].dateTime.buffer);   
-        } else {
-            fprintf(fileKicauan, "%s\n", kicauan.buffer[i].dateTime.buffer);
-        }
+        fprintf(fileKicauan, "\n%d", kicauan.buffer[i].IDKicau);
+        fprintf(fileKicauan, "\n%s", kicauan.buffer[i].text.buffer); 
+        fprintf(fileKicauan, "\n%d", kicauan.buffer[i].like);
+        fprintf(fileKicauan, "\n%s", user.contents[kicauan.buffer[i].IDuser].name.buffer); 
+        fprintf(fileKicauan, "\n%s", kicauan.buffer[i].dateTime.buffer);   
         
     }
     fclose(fileKicauan);
